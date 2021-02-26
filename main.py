6cea584e3be6fadab0f762a4f90a53b111911b8c
Utsav Dey <utsavdey@outlook.com>
2021-02-26 21:59:12 +0530

    Back propagation added.

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
 print('Hello World')
 import numpy as np
 import math
+import copy
 
 last = 2
 network = []
+gradient = []
 input = []
 
 
 def output_grad(y_hat, label):  # grad w.r.t out actiovation
-    e = np.zeros(y_hat.shape[0])
-    e[label] = 1 / y_hat[label]
-    return -e
+    temp = np.zeros_like(y_hat)
+    temp[label] = -1 / y_hat[label]
+    return temp
 
 
 def last_grad(y_hat, label):  # grad w.r.t out last layer
-    e = np.zeros(y_hat.shape[0])
-    e[label] = 1 - y_hat[label]
-    return -e
+    temp = np.copy(y_hat)
+    temp[label] = temp[label] - 1
+    return temp
 
 
-def a_grad(h_grad_, active_grad_):  # grad w.r.t out a_i's layer
-    return h_grad_.multiply(active_grad_)  # hadamard multiplication
+def a_grad(layer):  # grad w.r.t out a_i's layer
+    active_grad_ = np.multiply(network[layer]['h'], 1 - network[layer]['h'])
+    return np.multiply(gradient[layer]['h'],active_grad_)  # hadamard multiplication
 
 
-def h_grad(w, a_grad_):  # grad w.r.t out h_i layer
-    return w.transpose() @ a_grad_
+def h_grad(layer):  # grad w.r.t out h_i layer
+    network[layer]['weight'].transpose()
+    return network[layer + 1]['weight'].transpose() @ gradient[layer + 1]['a']
 
 
 def w_grad(a_grad_, h_k_1):
@@ -66,11 +70,17 @@ def train(x, y):
     loss = -1 * np.log(network[n - 1]['h'][y])
     # forward propagation ends
     print(loss)
+    # backpropagation starts
+    gradient[n - 1]['h'] = output_grad(network[n - 1]['h'], y)
+    gradient[n - 1]['a'] = last_grad(network[n - 1]['a'], y)
+    for i in range(n - 2, 0, -1):
+        gradient[i]['h'] = h_grad(layer=i)
+        gradient[i]['a'] = a_grad(layer=i)
 
 
 def master(layers, neuron_each_layer, k, x, y):
     n = neuron_each_layer
-    for i in range(layers):
+    for i in range(layers):  # making basic structure
         layer = {}
         if i == layers - 1:
             n = k
@@ -81,6 +91,8 @@ def master(layers, neuron_each_layer, k, x, y):
         layer['h'] = np.ones((n, 1))
         layer['a'] = np.ones((n, 1))
         network.append(layer)
+    global gradient
+    gradient = copy.deepcopy(network)  # structure copy
     train(x=x, y=y)
 
 
