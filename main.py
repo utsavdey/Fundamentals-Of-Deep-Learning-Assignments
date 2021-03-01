bb00d91ff845f91b73bd099247abc2637ffc614f
ArupDas15 <das12997@gmail.com>
2021-03-01 20:43:23 +0530

    Added comments to improve readability of code.

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+"""Implement Feed Forward neural network where the parameters are
+   number of hidden layers and number of neurons in each hidden layer"""
+
 import copy
 from keras.datasets import fashion_mnist
 from grad import *
 from activation import *
-# get training and testing vectors
+""" get training and testing vectors
+    Number of Training Images = 60000
+    Number of Testing Images = 10000 """
 (trainX, trainy), (testX, testy) = fashion_mnist.load_data()
+
+
 last = 2
 network = []
 gradient = []
@@ -53,11 +60,11 @@ def descent(eta, layers, number_of_data_points):
         network[i]['weight'] -= (eta / float(number_of_data_points)) * gradient[i]['weight']
         network[i]['bias'] -= (eta / float(number_of_data_points)) * gradient[i]['bias']
 
-
+# 1 epoch = 1 pass over the data
 def train(datapoints, epochs, labels, f):
     n = len(network)  # number of layers
     # f = len(datapoints[0])  # number of features
-    d = len(datapoints)
+    d = len(datapoints)  # number of data points
     # forward propagation
     print(datapoints[0].shape)
     for i in range(epochs):
@@ -76,32 +83,38 @@ def train(datapoints, epochs, labels, f):
         # forward propagation ends
 
 
-def master(layers, neuron_each_layer, epochs, k, x, y):
-    n = neuron_each_layer
+def master(layers, neurons_in_each_layer, epochs, k, x, y):
+    n = neurons_in_each_layer
+
+    """intializing number of input features per datapoint as 784, 
+           as dataset consists of 28x28 pixel grayscale images """
     n_features = 784
+
     for i in range(layers):  # making basic structure
-        layer = {}
+
+        layer = {} #Initialize an Empty Dictionary: layer
 
         if i == 0:  # Weight matrix depends on number of features in the first layer
             layer['weight'] = np.random.normal(size=(n, n_features))
             glorot = n_features
         elif i == layers - 1:  # special handling for the last layer.
             n = k
-            layer['weight'] = np.random.normal(size=(n, neuron_each_layer))
-            glorot = neuron_each_layer
+            # Create an array of size [number of classes * neurons last hidden layer] and fill it with random values from a Gaussian Distribution having 0 mean and 1 S.D.
+            layer['weight'] = np.random.normal(size=(n, neurons_in_each_layer))
+            glorot = neurons_in_each_layer
         else:
-            layer['weight'] = np.random.normal(size=(n, n))
+            layer['weight'] = np.random.normal(size=(n, n)) # Assuming the number of neurons in every hidden layer is the same
             glorot = n
         layer['weight'] = layer['weight'] * math.sqrt(
             1 / float(glorot))  # glorot inittialization. Vanishing and exploding gradient.
-        layer['bias'] = np.random.rand(n, 1)
-        layer['h'] = np.ones((n, 1))
+        layer['bias'] = np.random.rand(n, 1) #initialise a 1-D array of size n with random samples from a uniform distribution over [0, 1).
+        layer['h'] = np.ones((n, 1)) #initialises a 1-D array of size n and type float with element having valuue as 1.
         layer['a'] = np.ones((n, 1))
         network.append(layer)
     global gradient
-    gradient = copy.deepcopy(network)  # structure copy
+    gradient = copy.deepcopy(network)  # structure copy. The changes made to a copy of network will not reflect in the original network.
     train(datapoints=trainX, labels=trainy, epochs=epochs, f=n_features)
 
 
-master(layers=3, neuron_each_layer=3, epochs=6, k=10, x=trainX, y=trainy)
+master(layers=3, neurons_in_each_layer=3, epochs=6, k=10, x=trainX, y=trainy)
 
