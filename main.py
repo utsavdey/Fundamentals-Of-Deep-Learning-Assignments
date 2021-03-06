cf409d116c5f9ca80e9437c35c4faf4362d466c4
utsavdey <deyyutsav@gmail.com>
2021-03-06 09:46:34 +0530

    Changed main.py to handle new activation.py

--- main.py
+++ main.py
@@ -29,9 +29,9 @@ def forward_propagation(n, x):
         else:
             network[i]['a'] = network[i]['weight'] @ network[i - 1]['h'] + network[i]['bias']
         if i == n - 1:
-            network[i]['h'] = activation_function(network[i]['a'], softmax)  # last layer
+            network[i]['h'] = activation_function(network[i]['a'], context=network[i]['context'])  # last layer
         else:
-            network[i]['h'] = activation_function(network[i]['a'], sigmoid)
+            network[i]['h'] = activation_function(network[i]['a'], context=network[i]['context'])
 
 
 def backward_propagation(number_of_layers, x, y, number_of_datapoint, clean=False):
@@ -131,23 +131,23 @@ def add_layer(number_of_neurons, context, input_dim=None):
    in every layer and then start the training process"""
 
 
-def master(layers, neurons_in_each_layer, epochs,input_dim, output_dim, x, y):
+def master(layers, neurons_in_each_layer, epochs, output_dim, x, y):
     n = neurons_in_each_layer
 
     """intializing number of input features per datapoint as 784, 
        since dataset consists of 28x28 pixel grayscale images """
-
-    #adding layers
-    add_layer(number_of_neurons=16,context='sigmoid',input_dim=input_dim)
-    add_layer(number_of_neurons=8,context='sigmoid')
-    add_layer(number_of_neurons=output_dim,context='softmax')
+    n_features = 784
+    # adding layers
+    add_layer(number_of_neurons=16, context='sigmoid', input_dim=784)
+    add_layer(number_of_neurons=8, context='sigmoid')
+    add_layer(number_of_neurons=output_dim, context='softmax')
 
     global gradient
-    """Copy the structure of of network to gradient nd transient gradient."""
+    """Recursively make a copy of network. Changes made to the copy will not reflect in the original network."""
     gradient = copy.deepcopy(network)
     global transient_gradient
     transient_gradient = copy.deepcopy(network)
-    train(datapoints=trainX, labels=trainy, epochs=epochs, f=input_dim)
+    train(datapoints=trainX, labels=trainy, epochs=epochs, f=n_features)
 
 
-master(layers=3, neurons_in_each_layer=8, epochs=50, input_dim=784,output_dim=10, x=trainX, y=trainy)
+master(layers=3, neurons_in_each_layer=8, epochs=50, output_dim=10, x=trainX, y=trainy)
